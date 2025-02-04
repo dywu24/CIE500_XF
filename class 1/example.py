a967c33bce8b0078b13c40fc14c32f9a43dae801
dywu24 <dwu24@buffalo.edu>
2025-02-04 17:44:26 -0500

    Initial commit

--- class 1/example.py
+++ class 1/example.py
@@ -1,2 +1,15 @@
-print("hello world")
-print("Dongyang")
+class car:
+    def __init__ (self , color , model):
+        self.color = color
+        self.model = model
+
+    def mymodel(self):
+        print(f"car model is {self.model}")
+
+        
+if __name__ == "__main__":
+    var1 = 0
+    a = car("black","mini")
+    b = car("white","nissan")
+    a.mymodel()
+    print(a.color,b.model)
